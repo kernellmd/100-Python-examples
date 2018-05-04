6417b8e5ae10f53672b5e58f9a8871a1bf65c748
kernellmd <KernelLmd@LMD.localdomain>
2018-05-04 10:46:06 +0800

    add example019

--- example019.py
+++ example019.py
@@ -1,7 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3.6
+# -*  c ding: utf-8 -*-
 """
-题目描述：一个数如果恰好等于它的因子之和，这个数就称为"完数"。
-          例如6=1＋2＋3.编程找出1000以内的所有完数。
-程序分析：请参照程序Python 练习实例14。
+题目描述：一个数如果它所有的真因子（即除了自身以外的约数）的和，恰好等于它本身，则
+          称该数为完数。例如6=1＋2＋3。编程找出1000以内的所有完数。
+程序分析：利用for循环求出所有约数，利用sum计算所有约数之和是否为原整数
 """
+#原始程序
+#求约数，并将所有的约数装到列表中返回
+def divisor(n):
+    L = [1]
+    for i in range(2, n):  #还有优化空间，循环到 ceil(n / 2) + 1 就可以
+        if n % i == 0:
+            L.append(i)
+        else:
+            continue
+    return L
+
+#判断一个数是否为完全数
+def is_perfect_number(var):
+    tmp = divisor(var)
+    if var == sum(tmp):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    for i in range(2, 1000):
+        if is_perfect_number(i):
+            print(i)
+        else:
+            continue
