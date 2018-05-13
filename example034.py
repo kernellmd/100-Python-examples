02a8b1a7d5c9b7823285e8f185c618cfd16ea6a7
kernellmd <KernelLmd@163.com>
2018-05-13 10:05:47 +0800

    add example034

---
+++ example034.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.6
+#-*- coding: utf-8 -*-
+
+"""
+题目描述：练习函数调用。
+程序分析：呵呵！真无聊
+"""
+
+#unicode编码的函数名，pythn3支持，
+#python2不支持
+def 呵呵():
+    print("呵呵！" * 3)
+
+if __name__ == "__main__":
+    呵呵()
