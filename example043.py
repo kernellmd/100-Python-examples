8582f918e633e5428a606de04cd451911e2c298f
kernellmd <KernelLmd@163.com>
2018-05-22 17:48:16 +0800

    add example043

---
+++ example043.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+'''
+题目描述：利用类变量代替静态变量
+'''
+class Num:
+    nNum = 1
+    def inc(self):
+        self.nNum += 1
+        print('nNum = %d' % self.nNum)
+
+if __name__ == '__main__':
+    nNum = 2
+    inst = Num()
+    for i in range(3):
+        nNum += 1
+        print('The num = %d' % nNum)
+        inst.inc()
